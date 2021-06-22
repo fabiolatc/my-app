c339b479484a6bb3b2bec1d53dab3156c8c7a789
fabiolatc <61229208+fabiolatc@users.noreply.github.com>
2021-06-22 08:43:35 +0200

    APp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
+export default function App() {
+  let [city, cityAnswer] = useState("");
+  let [text, changeText] = useState("");
+  let [descriptiontext, changeDescriptiontext] = useState("");
+  let [temperaturetext, changeTemperaturetext] = useState("");
+  let [humiditytext, changeHumiditytext] = useState("");
+  let [windtext, changeWindtext] = useState("");
+  let [icontext, changeIcontext] = useState("");
+  function updateCity(event) {
+    cityAnswer(event.target.value);
+  }
+  function searchCity(event) {
+    let APIkey = "3c7e72471b038017abb118fddfa1d953";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=metric`;
+    axios.get(url).then(cityTemperature);
+    event.preventDefault();
+    function cityTemperature(response) {
+      changeText(`${city}`);
+      changeTemperaturetext(
+        `Temperature: ${Math.round(response.data.main.temp)}°C`
+      );
+      changeDescriptiontext(
+        `Description: ${response.data.weather[0].description}`
+      );
+      changeHumiditytext(`Humidity: ${response.data.main.humidity}%`);
+      changeWindtext(`Wind: ${response.data.wind.speed} km/h`);
+      changeIcontext(
+        <img
+          src={`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`}
+        />
+      );
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+      <form onSubmit={searchCity}>
+        <input type="text" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
+      <h2>{text}</h2>
+      <ul>
+        <li>{descriptiontext}</li>
+        <li>{temperaturetext}</li>
+        <li>{humiditytext}</li>
+        <li>{windtext}</li>
+        <li>{icontext}</li>
+      </ul>
     </div>
   );
 }
-
-export default App;
